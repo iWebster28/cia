5167d9608eee0096f5becd3f55a1548c9570adc4
iWebster28 <iWebster28@users.noreply.github.com>
2021-01-16 17:28:08 -0500

    Collab session on getting flask and react implemented - fetch works to grab demo database from flask /data page

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 * Setup virtual env in root folder: ```python3 -m venv env```
 * Run ```./venv``` to enter virtual env
 * ```pip install -r ./backend/flask/requirements.txt```
+
+## Start the Flask Server
+* cd to the backend flask folder
+* Run ```python3 app.py 5012```, where 5012 should also be in App.js in the react frontend, http://localhost:5012/data
+
+## Start the react app
+* Run ```npm install``` for dependencies
+* Run ```npm start``` in frontend/cia dir
--- frontend/cia/src/App.js
+++ frontend/cia/src/App.js
@@ -1,29 +1,65 @@
 import './App.css';
+import React, {useEffect, useState} from "react";
 import { Button } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
-const makeAndModel = [
-  { make: 'Alfa Romeo', model: 'GT V6 2.5'},
-  { make: 'Audi', model: '4000'},
-  { make: 'BMW', model: '3 Series' },
-];
+//To be replaced
+// const makeAndModel = [
+//   { make: 'Alfa Romeo', model: 'GT V6 2.5', vehicle_id: 1984},
+//   { make: 'Audi', model: '4000', vehicle_id: 19854},
+//   { make: 'BMW', model: '3 Series', vehicle_id: 19854},
+// ];
+const makeAndModel = [];
+
+
+// const barrels = 59; //temp demo
+
+
+//Eventually move this to Components folder
+
+function FetchFlask({ url }) {
+  const [data, setData] = useState(null); 
+  useEffect(() => { //  useEffect fetches from 'url' and sets data
+    fetch(`${url}`, {method: "GET", headers: {"Content-Type": "application/json", "Accept": "application/json"}})
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .then(setData)
+      .catch(console.error);
+  }, []);
+
+  console.log('hi');
+
+  // Separate the make and model information to put in the combo box
+
+  if (data) {
+    return (
+      <div>
+      <h1> {data["0"]} was fetched</h1>
+      <h1>You'll consume -- barrels of fuel per annum.</h1>
+      <h1>That's the same as --some equivalent surprising comparison--</h1>
+      </div>
+    );
+  }
+  return null;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>CIA</h1>
+        <h5>Car Impact Associator</h5>
+
+        <FetchFlask url="http://localhost:5012/data" />
         
-        <Autocomplete
-          id="combo-box-demo"
-          options={makeAndModel}
-          getOptionLabel={(option) => option.make + ' ' + option.model}
-          style={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-        />
+        {/* <ComboBox list={makeAndModel.map(x => x['make'] + ' ' + x['model'])} />
+        <ComboBox list={makeAndModel.map(x => x['year'])} /> */}
+        <ComboBox list={makeAndModel}/>
+
 
         <br></br>
-        <Button color="primary">Find me a car</Button>
+        <Button variant="contained" color="primary">Find impact</Button>
 
         <br></br>
         <a
@@ -37,6 +73,16 @@ function App() {
       </header>
     </div>
   );
+}
+
+function ComboBox(props) {
+  return <Autocomplete
+    id="car-combo-box"
+    options={makeAndModel}
+    getOptionLabel={(option) => option.make + ' ' + option.model}
+    style={{ width: 300 }}
+    renderInput={(params) => <TextField {...params} label={"Vehicle Selection"} variant="outlined" />}
+  />;
 }
 
 export default App;
