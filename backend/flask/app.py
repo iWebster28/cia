05266f52e420b4e7b597b3bbacc31c12569124d3
iWebster28 <iWebster28@users.noreply.github.com>
2021-01-16 01:59:37 -0500

    flask app template

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return "Index page"
 
+@app.route('/data', methods=['GET']) 
+def get_data():
+    """
+    Extract data from the SQL database upon a GET request from the React app
+    """
+    return "This should grab info from the db"
 
 if __name__ == '__main__':
     app.run()
