598b038bd998bd1bd0c230095ca2584e83a97ad4
iWebster28 <iWebster28@users.noreply.github.com>
2021-01-16 20:25:28 -0500

    Componentize FetchFlask

--- frontend/cia/src/App.js
+++ frontend/cia/src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
-import React, {useEffect, useState} from "react";
 import { Button } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+
+import FetchFlask from './Components/FetchFlask';
 
 //To be replaced
 // const makeAndModel = [
@@ -10,45 +9,6 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 //   { make: 'Audi', model: '4000', vehicle_id: 19854},
 //   { make: 'BMW', model: '3 Series', vehicle_id: 19854},
 // ];
-
-
-// const barrels = 59; //temp demo
-
-
-//Eventually move this to Components folder
-
-function FetchFlask({ url }) {
-  const [data, setData] = useState(null); 
-  useEffect(() => { //  useEffect fetches from 'url' and sets data
-    fetch(`${url}`, {method: "GET", headers: {"Content-Type": "application/json", "Accept": "application/json"}})
-      .then(res => res.json())
-      .then(data => setData(data))
-      .catch(console.error);
-  }, []);
-
-  // console.log('hi' + JSON.stringify(data));
-
-  // Separate the make and model information to put in the combo box
-  if (data) {
-    // const makeAndModel = data[0].map(x => [x.make, x.model, x.vehicle_id]);
-    // console.log(makeAndModel);
-    console.log(data[0]);
-    return (
-      <div>
-        <ComboBox list={data[0]} />
-        {/* <h1> {makeAndModel}</h1> */}
-
-
-{/* This should be moved to later. i.e. on button click, add this text. Use data[0] indexed at the corresponding vehicle id, and extract the gas mileage/etcc....
-        <h1>You'll consume -- barrels of fuel per annum.</h1>
-        <h1>That's the same as --some equivalent surprising comparison--</h1> */}
-      
-      
-      </div>
-    );
-  }
-  return null;
-}
 
 function App() {
   return (
@@ -70,20 +30,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Git Repo
-        </a>-
+        </a>
       </header>
     </div>
   );
 }
 
-function ComboBox(props) {
-  return <Autocomplete
-    id="car-combo-box"
-    options={props.list}
-    getOptionLabel={(option) => option.make + ' ' + option.model + ' - ' + option.vehicle_id}
-    style={{ width: 300 }}
-    renderInput={(params) => <TextField {...params} label={"Vehicle Selection"} variant="outlined" />}
-  />;
-}
 
 export default App;
---
+++ frontend/cia/src/Components/FetchFlask.js
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from "react";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { createFilterOptions } from '@material-ui/lab/Autocomplete';
+
+
+function FetchFlask({ url }) {
+    const [data, setData] = useState(null); 
+    useEffect(() => { //  useEffect fetches from 'url' and sets data
+      fetch(`${url}`, {method: "GET", headers: {"Content-Type": "application/json", "Accept": "application/json"}})
+        .then(res => res.json())
+        .then(data => setData(data))
+        .catch(console.error);
+    }, []);
+  
+    // console.log('hi' + JSON.stringify(data));
+  
+    // Separate the make and model information to put in the combo box
+    if (data) {
+      // const makeAndModel = data[0].map(x => [x.make, x.model, x.vehicle_id]);
+      // console.log(makeAndModel);
+      console.log(data[0]);
+      return (
+        <div>
+          <ComboBox list={data[0]} />
+          {/* <h1> {makeAndModel}</h1> */}
+  
+
+        {/* This should be moved to later. i.e. on button click, add this text. Use data[0] indexed at the corresponding vehicle id, and extract the gas mileage/etcc....
+          <h1>You'll consume -- barrels of fuel per annum.</h1>
+          <h1>That's the same as --some equivalent surprising comparison--</h1> */}
+        
+        
+        </div>
+      );
+    }
+    return null;
+}
+
+function ComboBox(props) {
+    return <Autocomplete
+      id="car-combo-box"
+      options={props.list}
+      getOptionLabel={(option) => option.make + ' ' + option.model + ' - ' + option.vehicle_id}
+      style={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label={"Vehicle Selection"} variant="outlined" />}
+    />;
+}
+
+export default FetchFlask;
